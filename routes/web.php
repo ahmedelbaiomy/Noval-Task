26182129811412cccfb36ca6dd0fcbb74bf2900b
ahmedelbaiomy <ahmedelbaiomy40@gmail.com>
2023-01-15 09:48:24 +0200

    Blog

--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,7 @@ Route::group(['middleware'=>'auth'],function(){
     Route::post('posts/store',[PostController::class,'store'])->name('posts.store');
     Route::get('/posts/{id}',[PostController::class,'show'])->name('posts.show');
     Route::get('posts/edit/{id}',[PostController::class,'edit'])->name('posts.edit');
-    Route::post('posts/update/{id}',[PostController::class,'update'])->name('posts.update');
+    Route::put('posts/update/{id}',[PostController::class,'update'])->name('posts.update');
     Route::delete('posts/destroy/{id}',[PostController::class,'destroy'])->name('posts.destroy');
 //    +++++++++++++ end posts ++++++++++++
 //    +++++++++++++ comments ++++++++++++++++
